7c09afb67cb671b66f7302c6bc4e09c58fc44ece
steph-MAUREL-CHARTROU <58855879+steph-MAUREL-CHARTROU@users.noreply.github.com>
2021-01-19 09:07:43 +0100

    Add files via upload

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST     : 'localhost',
+    USER     : 'root', 
+    PASSWORD : '', 
+    DB       : 'classroom_project',
+    DIALECT : 'mysql'
+}
---
+++ config/test/liste.users.js
@@ -0,0 +1,20 @@
+module.exports = users = 
+[
+    {
+        "email" : "Sylvain.lardiere@gmail.com",
+        "password" : "sylvain"
+    },
+    {
+        "email" : "quentin.lardiere@gmail.com",
+        "password" : "quentin"
+    },
+    {
+        "email" : "clement.lardiere@gmail.com",
+        "password" : "clement"
+    },
+    {
+        "email" : "lucie.lardiere@gmail.com",
+        "password" : "lucie"
+    }
+]
+
